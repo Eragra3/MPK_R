816d1121abf9dae3f7ad30341861e3d5b0a01fa4
Daniel Bider <bider.daniel@gmail.com>
2017-03-08 23:51:47 +0100

    Created some graph. Yay

--- List1/scripts/time.R
+++ List1/scripts/time.R
@@ -1,1 +1,12 @@
-res_h <- aggregate(mpk$timestamp, as.numeric(format(mpk$timestamp, "%H")), sum)
+
+p <- head(mpk$timestamp)
+f <- format(p, "%H")
+h <- as.numeric(f)
+
+ps <- mpk$timestamp
+fs <- format(ps, "%H")
+hs <- as.numeric(fs)
+
+res_h <- aggregate(mpk$timestamp, list(hs), length)
+plot(res_h, type = 'b')
+plot(res_h, type = 'h')
